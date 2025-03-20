a6d85761af74f8e3ab155b2881d6fbd8309d1b97
Janosch Moor <janosch.moor@gmail.com>
2025-03-20 16:18:06 +0100

    tamas interface

---
+++ app/components/charts/interfaces.ts
@@ -0,0 +1,31 @@
+import {AxisChartDataList, NonAxisChartDataList} from './ApexSeriesConverter';
+
+
+type OrchestratorData = {
+    type: "line" | "candle" | "news" | "kpi" | "pie" | "table",
+    title: string,
+    data: LineData | CandleData | PieData | KpiData | NewsData | TableData
+}[]
+
+type KpiData = {
+    number: number
+    footer: string
+}
+
+type NewsData = {
+    content: string,
+    source: string
+}
+
+type TableData = {
+    header: string[],
+    content: {
+        [key: string]: string
+    }[]
+}
+
+type LineData = AxisChartDataList;
+type CandleData = AxisChartDataList;
+type PieData = NonAxisChartDataList;
+
+//const data: TableData = {header: ["test"], content: [{"test": "hallo"}, {"test": "hallo"}, {"test": "micha"}]}
